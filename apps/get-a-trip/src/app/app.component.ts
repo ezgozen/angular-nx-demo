7c70e5e8e512c572cb5d67b179a3a19382932c17
Ezgi <ezg.ozen93@gmail.com>
2022-06-23 12:02:58 +0300

    initial commit

---
+++ apps/get-a-trip/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'demo-project-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'get-a-trip';
+}
